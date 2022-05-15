ed773e9f43169afbb601d843958bb176325dadd9
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-05-15 20:06:51 -0300

    feat: Initial commit.

---
+++ traited.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env ksh93
+
+# Traité de la tabula: HTML (and maybe PDF?) generator for Markdown
+# documentation in a "tabula-style".
+# A mere hack, I am not sure if this will work in the end.
+
+
+# Include the configuration file.
+. /usr/etc/traite.conf
+
+# Include some libraries that we will be using. 
+. "${ADDERE}"/posix-alt.shi
+
+# Cuz We're real
+documents="$(realpath "$DOCUMENTS")"
+www_output_directory="$(realpath "$WWW_DIRECTORY")"
+
+# This will be used when we will generate a custom HTML footer for the document
+# in question.
+
+ksh_version=${.sh.version}
+pandoc_version=$(pandoc -v | sed 1q)
+kernel_name=$(uname -s)
+kernel_release=$(uname -r)
+machine_host=$(uname -n)
+
+function main {
+	generate_html_documents
+}
+
+function generate_html_documents {
+	# Cheap hack for getting a list for all the documents to be used.
+	# Probably isn't bad-name safe.
+	docdir=( $(cd "$documents"; echo *) )
+	output="${OUTPUT:-index.html}"
+	owd="$PWD"
+	current_time=$(date +%F)
+
+	for ((i=0; i < $(n ${docdir[@]}); i++)); do
+		# First of all: check if tabula.conf exist in all the
+		# directories; if not, it's not a tabula.
+		test -f "$documents/${docdir[$i]}/tabula.conf" \
+			|| { printL 'ERROR: %s is not a tabula\n' \
+		       	"${docdir[$i]}"; continue; } \
+			&&  cd "$documents/${docdir[$i]}" \
+		&& printL 'ACTION: Entered directory %s\n' "$documents/${docdir[$i]}"
+		printL 'ACTION: Reading tabula.conf\n'
+		. ./tabula.conf
+		printL 'INFO:\nTitle: %s\nNumber of files: %s\nFiles: %s\n' \
+			"$title"  $(n ${files[@]}) "${files[@]}"
+
+		# Determine absolute paths for files listed in tabula.conf
+		for ((j=0; j < $(n ${files[@]}); j++)); do
+			realfiles+=$(realpath "${files[$j]}")
+		done
+		printL 'ACTION: realpath'\''d files.\n'
+		printL 'INFO:\nOriginal files: %s\nrealpath'\''d files: %s\n' \
+			"${files[@]}" "${realfiles[@]}"
+	
+		deploy_directory="$www_output_directory/${docdir[$i]}"
+		printL 'ACTION: Creating directory %s for the compiled files\n' \
+		       	"$deploy_directory"
+		test -d "$deploy_directory" -a -w "$deploy_directory" \
+			|| mkdir -pv "$deploy_directory" 2>&1 | tee "$LOG"
+		cd "$deploy_directory" \
+		&& printL 'ACTION: Entered directory %s\n' "$deploy_directory"
+		
+		printL 'ACTION: Compiling documentation\n'
+		md2html "${realfiles[@]}" "$deploy_directory/$output" "$title" \
+			"$lang"
+		
+		# Go back to that same old place...
+	        # Oh, sweet home old working directory
+		cd "$owd"
+	done
+}
+
+# Just a boilerplate for calling Pandoc, of course
+function md2html {
+	echo "$PWD" $1 $2 $3
+	files="$1"
+	output="$2"
+	title="$3"
+	lang="$4"
+	pandoc --highlight-style="$THEME" \
+		--metadata title="$title" \
+			--metadata lang="$lang"\
+		"$files" \
+		--output "$output" \
+		-s --pdf-engine="${pdfengine:-xelatex}" \
+		--verbose \
+		${pandoc_opts[@]} \
+		2>&1 | tee "$LOG"
+}
+
+#function generate_html_footer {
+#	tempfooter=$(mktemp footer.XXXXXX.html)
+#}
+
+# printL - print to log
+# I did not put this into another file (library) because I don't think it's
+# worth for now.
+
+function printL { 
+	printf "$@" 2>&1 | tee "$LOG"
+
+}
+
+main $@
